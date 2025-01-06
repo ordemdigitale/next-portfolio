bd0bf6a1a60808af4c2bc866c8d926901e9783be
Ordem <ordemdigitale@gmail.com>
2025-01-06 14:21:10 +0000

    Updated app folder + created Navbar component

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const MainLayout = ({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) => {
+  return (
+    <>
+      
+      {children}
+    </>
+  )
+}
+
+export default MainLayout
---
+++ components/Navbar.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <header>
+      <nav>
+        <span>Logo</span>
+        <span>Menu</span>
+        <span>CTA</span>
+      </nav>
+    </header>
+  )
+}
+
+export default Navbar
