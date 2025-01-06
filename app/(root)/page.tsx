9d3f05cf865b993f0a354dcf11f5191dfa2e05dd
Ordem <ordemdigitale@gmail.com>
2025-01-06 14:34:05 +0000

    Added shadcn button to homepage + installed tailwindcss animate

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,12 @@
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <>
       <h1>Home page</h1>
+      <span>Shadcn button</span>
+      <br />
+      <Button>I am a shadcn button</Button>
     </>
   );
 }
